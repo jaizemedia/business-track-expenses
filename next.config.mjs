c96fd0d8ea5df101289075842f22aed4c499b823
jaizemedia <info@jaizemedia.com>
2026-01-05 14:33:25 +0000

    okay

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
